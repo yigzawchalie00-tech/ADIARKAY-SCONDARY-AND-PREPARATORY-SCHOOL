1ded06f57100ec32b169f6c6b9b87d8664b3ac68
yigzawchalie00-tech <yigzawchalie00@gmail.com>
2026-09-21 20:29:25 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://dckvhtscevqbtjdiifui.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_GkPKEAjaAIIhr3DQrpHyfg_twgqQw97';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const ADMIN_PASSWORD = 'adiarkay2017';
